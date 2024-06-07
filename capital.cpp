19fe6f3d637f3d6820f34033ef99b792a1ccc5b1
gheux <ialm0s9@outlook.com>
2024-06-07 23:16:07 +0800

    Modify capital.cpp

--- capital.cpp
+++ capital.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Turn start important the choose these range down.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
