2636c98240f2b1d2589ab7390a2007d731ec9418
gheux <ialm0s9@outlook.com>
2024-06-24 11:36:56 +0800

    Modify right.c

--- right.c
+++ right.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# A tree society economic heavy economic magazine win.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
