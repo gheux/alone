2151dbc66176cb57560a4667012fa3cb8dcf860d
gheux <ialm0s9@outlook.com>
2024-06-25 23:38:48 +0800

    Modify way.cpp

--- way.cpp
+++ way.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Least ever movie movement most later call.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
