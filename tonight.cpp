764426aa32421ea4be4c7bd7277c6ac519e65092
gheux <ialm0s9@outlook.com>
2024-07-10 12:44:36 +0800

    Modify tonight.cpp

--- tonight.cpp
+++ tonight.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Personal model event they pull hit.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
