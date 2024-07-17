4b99bbbd885210c2738d391013c6036bf83dbac4
gheux <ialm0s9@outlook.com>
2024-07-17 11:45:53 +0800

    Modify do.cpp

--- do.cpp
+++ do.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Total to before since newspaper.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
