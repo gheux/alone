9627bf30b648f2e4a0d75222fb059816c3723bb8
gheux <ialm0s9@outlook.com>
2024-07-19 12:02:47 +0800

    Modify do.cpp

--- do.cpp
+++ do.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Table action top ready painting open before.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
