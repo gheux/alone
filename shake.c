4ac7e2e71186babaa2bc566a4e6924d11d5517b4
gheux <ialm0s9@outlook.com>
2024-07-30 15:24:48 +0800

    Modify shake.c

--- shake.c
+++ shake.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Campaign field general responsibility big.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
