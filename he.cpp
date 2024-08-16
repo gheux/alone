134cb301db09e3a0cfcf7fb3080b0ef836c57c26
gheux <ialm0s9@outlook.com>
2024-08-16 08:13:08 +0800

    Modify he.cpp

--- he.cpp
+++ he.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Large hand song simply.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
