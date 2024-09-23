530d89268d0acc66c0f749b463a0706532a01fbb
gheux <ialm0s9@outlook.com>
2024-09-23 12:04:04 +0800

    Modify option.c

--- option.c
+++ option.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Sister better recently check whole drug west perhaps.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
