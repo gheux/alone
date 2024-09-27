0a65f752206b2c3342fb74c21b8fefadcf735507
gheux <ialm0s9@outlook.com>
2024-09-27 12:46:41 +0800

    Modify professional.cpp

--- professional.cpp
+++ professional.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Candidate anyone firm popular.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
