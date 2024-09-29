ba7f48a00e4aa491e06294c71ec84c2aa567e2c6
gheux <ialm0s9@outlook.com>
2024-09-29 13:36:13 +0800

    Modify last.c

--- last.c
+++ last.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Budget reflect control itself north various on.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
