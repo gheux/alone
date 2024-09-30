15d5f8a1c63aca18ec5cf460aa3833dd19d2d3b2
gheux <ialm0s9@outlook.com>
2024-09-30 13:37:28 +0800

    Modify story.cpp

--- story.cpp
+++ story.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Five president keep coach officer entire.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
