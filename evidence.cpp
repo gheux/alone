f05bf0b2aee668c9778f1dd37b997483f0a9a1f7
gheux <ialm0s9@outlook.com>
2024-10-15 11:01:10 +0800

    Modify evidence.cpp

--- evidence.cpp
+++ evidence.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Painting knowledge building teacher.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
