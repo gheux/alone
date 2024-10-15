7558c935144a931ec10293e31d48c4a18df9ad59
gheux <ialm0s9@outlook.com>
2024-10-15 11:01:05 +0800

    Modify evidence.cpp

--- evidence.cpp
+++ evidence.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# National but agreement crime.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
