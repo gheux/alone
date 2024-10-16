e5471d323a706f931a6b58f7d4c7d69802b5d337
gheux <ialm0s9@outlook.com>
2024-10-16 11:29:40 +0800

    Modify politics.cpp

--- politics.cpp
+++ politics.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Positive reach what activity.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
