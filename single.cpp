4a6f4d9da72cc5f665098766757f92ba1947564f
gheux <ialm0s9@outlook.com>
2024-10-23 12:55:54 +0800

    Modify single.cpp

--- single.cpp
+++ single.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# System thousand I physical.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
