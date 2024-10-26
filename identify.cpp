2bbdc6ff6ffc65cafd2f4cecd1d8dd81f770890d
gheux <ialm0s9@outlook.com>
2024-10-26 13:45:19 +0800

    Modify identify.cpp

--- identify.cpp
+++ identify.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Adult seem detail create.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
