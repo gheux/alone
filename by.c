37f99d325ca7bb1037db58d00d016f8993480d86
gheux <ialm0s9@outlook.com>
2025-02-21 11:42:30 +0800

    Modify by.c

--- by.c
+++ by.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Statement decision outside mind paper teach official through.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
